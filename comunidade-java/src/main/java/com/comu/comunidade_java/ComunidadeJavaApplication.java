bf2164f89849bfa7e3e028f28c91487fc0fc6c0b
Guilherme Pereira <gfranciscosilvapereira@gmail.com>
2025-05-31 18:39:25 -0300

    criando ambiente

---
+++ comunidade-java/src/main/java/com/comu/comunidade_java/ComunidadeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.comu.comunidade_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunidadeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunidadeJavaApplication.class, args);
+	}
+
+}
---
+++ comunidade-java/src/test/java/com/comu/comunidade_java/ComunidadeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comu.comunidade_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunidadeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
